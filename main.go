14e8a7f019cd18895063805b9f140504df805f9c
amsen <ahph1380@gmail.com>
2023-07-29 20:01:43 +0330

    Add main

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"amsen20/DrStress/internal/load"
+	"amsen20/DrStress/internal/stress"
+	"flag"
+	"fmt"
+	"sync"
+)
+
+func main() {
+	pathPtr := flag.String("path", "", "path to stress load data")
+	loadData, err := load.GetData(*pathPtr)
+	if err != nil {
+		fmt.Printf("could not start stressing due to following error: %s\n", err.Error())
+	}
+
+	var wg *sync.WaitGroup
+	for _, API := range loadData.APIs {
+		wg.Add(1)
+		go func(api *stress.API) {
+			defer wg.Done()
+			api.StressAPI()
+		}(API)
+	}
+	wg.Wait()
+}
