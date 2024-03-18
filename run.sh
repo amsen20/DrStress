697c3e37ba9514911c550156156b3d9c7255c18c
sina shariati <sina.shariati@yahoo.com>
2024-03-18 11:50:34 +0330

    Feat: Add rollout restart after every scenario

--- run.sh
+++ run.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
+namespace="ecmus"
+scheduler_deployment_name="ecmus-scheduler"
+normal_scenarios_path="./scenarios/normal/*.json"
+wavy_scenarios_path="./scenarios/wavy/*.json"
+
 
 echo "building the DrStress binary"
 go build -o stress main.go
 echo "DrStress binary has successfully been built"
 
-normal_scenarios_path="./scenarios/normal/*.json"
-wavy_scenarios_path="./scenarios/wavy/*.json"
+
 function run_scenario() {
   echo "scenario name: $1"
   echo "start time: $(date '+%Y-%m-%d %H:%M:%S')"
-  ./stress --path $1
-  echo "end time: $(date +%Y%m%d%H%M%S)"
+#  ./stress --path $1
+  kubectl rollout restart $scheduler_deployment_name -n $namespace
+  echo "end time: $(date '+%Y-%m-%d %H:%M:%S')"
 }
 
 
 echo "running normal scenarios ..."
 for filename in $normal_scenarios_path; do
   run_scenario $filename
+  echo "going to sleep between scenarios (5m)"
   sleep 5m
 done
 echo "normal scenarios are successfully ran ..."
