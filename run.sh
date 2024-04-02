628e48b92b94684a1052d08136d4e89f27f59191
sina shariati <sina.shariati@yahoo.com>
2024-04-02 23:53:42 +0330

    chore: comment all other schedulers except ecmus

--- run.sh
+++ run.sh
@@ -10,21 +10,21 @@ source ./scripts/cloud-node.sh
 
 function run() {
   run_ecmus_scheduler > logs/ecmus/ecmus_scheduler.txt 2> logs/ecmus/ecmus_scheduler_err.txt
-  sleep 5m
-
-  run_default_scheduler > logs/kube_schedule/kube_schedule.txt 2> logs/kube_schedule/kube_schedule_err.txt
-  sleep 5m
-
-  run_random_scheduler > logs/random/random_scheduler.txt 2> logs/random/random_scheduler_err.txt
-  sleep 5m
-
-  run_biggest_edge_node_first_scheduler > logs/biggest-fitting/biggest_edge_node_first_scheduler.txt 2> logs/biggest-fitting/biggest_edge_node_first_scheduler_err.txt
-  sleep 5m
-
-  run_smallest_edge_node_first_scheduler > logs/smallest-fitting/smallest_edge_node_first_scheduler.txt 2> logs/smallest-fitting/smallest_edge_node_first_scheduler_err.txt
-  sleep 5m
-
-  run_cloud_node_first_scheduler > logs/cloud-first/run_cloud_first_scheduler.txt 2> logs/cloud-first/run_cloud_first_scheduler_err.txt
+#  sleep 5m
+#
+#  run_default_scheduler > logs/kube_schedule/kube_schedule.txt 2> logs/kube_schedule/kube_schedule_err.txt
+#  sleep 5m
+#
+#  run_random_scheduler > logs/random/random_scheduler.txt 2> logs/random/random_scheduler_err.txt
+#  sleep 5m
+#
+#  run_biggest_edge_node_first_scheduler > logs/biggest-fitting/biggest_edge_node_first_scheduler.txt 2> logs/biggest-fitting/biggest_edge_node_first_scheduler_err.txt
+#  sleep 5m
+#
+#  run_smallest_edge_node_first_scheduler > logs/smallest-fitting/smallest_edge_node_first_scheduler.txt 2> logs/smallest-fitting/smallest_edge_node_first_scheduler_err.txt
+#  sleep 5m
+#
+#  run_cloud_node_first_scheduler > logs/cloud-first/run_cloud_first_scheduler.txt 2> logs/cloud-first/run_cloud_first_scheduler_err.txt
 }
 
 run;
