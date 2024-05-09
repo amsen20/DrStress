5aec483c12ef8eafd6dd865ee25c13fdde92c186
sina shariati <sina.shariati@yahoo.com>
2024-05-09 20:36:51 +0330

    Feat: enable runner script for all algorithms in custom_k8s_scheduler

--- run.sh
+++ run.sh
@@ -20,19 +20,20 @@ function run() {
 #  run_default_scheduler > logs/kube_schedule/kube_schedule.txt 2> logs/kube_schedule/kube_schedule_err.txt
 #  sleep 5m
 #
-#  run_random_scheduler > logs/random/random_scheduler.txt 2> logs/random/random_scheduler_err.txt
-#  sleep 5m
-
-#  run_biggest_edge_node_first_scheduler > logs/biggest-fitting/biggest_edge_node_first_scheduler.txt 2> logs/biggest-fitting/biggest_edge_node_first_scheduler_err.txt
-#  sleep 5m
-#
-#  run_smallest_edge_node_first_scheduler > logs/smallest-fitting/smallest_edge_node_first_scheduler.txt 2> logs/smallest-fitting/smallest_edge_node_first_scheduler_err.txt
-#  sleep 5m
-#
-#  run_cloud_node_first_scheduler > logs/cloud-first/run_cloud_first_scheduler.txt 2> logs/cloud-first/run_cloud_first_scheduler_err.txt
-#  sleep 5m
-  run_ecmus_no_qos_scheduler > logs/ecmus-no-qos/ecmus-no-qos-scheduler.txt 2> logs/ecmus-no-qos-scheduler-err.txt
+  run_random_scheduler > logs/random/random_scheduler.txt 2> logs/random/random_scheduler_err.txt
   sleep 5m
+
+  run_biggest_edge_node_first_scheduler > logs/biggest-fitting/biggest_edge_node_first_scheduler.txt 2> logs/biggest-fitting/biggest_edge_node_first_scheduler_err.txt
+  sleep 5m
+
+  run_smallest_edge_node_first_scheduler > logs/smallest-fitting/smallest_edge_node_first_scheduler.txt 2> logs/smallest-fitting/smallest_edge_node_first_scheduler_err.txt
+  sleep 5m
+
+  run_cloud_node_first_scheduler > logs/cloud-first/run_cloud_first_scheduler.txt 2> logs/cloud-first/run_cloud_first_scheduler_err.txt
+  sleep 5m
+
+#  run_ecmus_no_qos_scheduler > logs/ecmus-no-qos/ecmus-no-qos-scheduler.txt 2> logs/ecmus-no-qos-scheduler-err.txt
+#  sleep 5m
 }
 
 run;
