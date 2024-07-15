00a9d0a02ec0dc21921e436f0f8af5c16547700c
sina shariati <sina.shariati@yahoo.com>
2024-07-15 15:23:31 +0330

    Feat: add setup_genny function

--- iterative.sh
+++ iterative.sh
@@ -12,6 +12,16 @@ source ./scripts/cloud-node.sh
 source ./scripts/ecmus-no-qos.sh
 
 iteration_count=10
+function setup_genny() {
+   cd genny
+
+   python3 -m venv .venv
+   source ./.venv/bin/activate
+   python3 -m pip install -r requirements.txt
+
+   cd ..
+}
+
 function create_scenario_files() {
   cd genny
   rm -rf ./out
@@ -27,21 +37,24 @@ function move_scenario_files() {
 }
 
 function run() {
+  mkdir logs/genny logs/move logs/ecmus logs/biggest-fitting logs/smallest-fitting logs/setup_genny;
+  setup_genny 1> logs/setup_genny/setup_genny.txt 2> logs/setup_genny/setup_genny_err.txt;
+
   for ((i=1; i<=$iteration_count;i++)) ; do
-    create_scenario_files;
-    move_scenario_files;
+    create_scenario_files 1> logs/genny/"genny_${i}.txt" 2> logs/ecmus/"genny_err_${i}.txt";
+    move_scenario_files 1> logs/move/"move_${i}.txt" 2> logs/move/"move_${i}_err.txt";
 
     run_ecmus_scheduler 1> logs/ecmus/"ecmus_scheduler_${i}".txt \
                         2> logs/ecmus/"ecmus_scheduler_${i}_err.txt";
-    sleep 5m;
+#    sleep 5m;
 
     run_biggest_edge_node_first_scheduler 1> logs/biggest-fitting/"biggest_edge_node_first_scheduler_${i}.txt" \
                                           2> logs/biggest-fitting/"biggest_edge_node_first_scheduler_err_${i}.txt";
-    sleep 5m;
+#    sleep 5m;
 
     run_smallest_edge_node_first_scheduler 1> logs/smallest-fitting/"smallest_edge_node_first_scheduler_${i}.txt" \
                                            2> logs/smallest-fitting/"smallest_edge_node_first_scheduler_err_${i}.txt";
-    sleep 5m;
+#    sleep 5m;
   done
 
 }
